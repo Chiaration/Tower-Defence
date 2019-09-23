c5ffd3061de13e4de1fb34721dda76480b72659e
Chiaration <45766347+Chiaration@users.noreply.github.com>
2019-09-23 15:23:10 +0100

    Added ExectureInEditMode

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorSnap : MonoBehaviour
+{
+
+    [SerializeField] [Range(0, 10)] int gridSize = 1;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 gridSnap;
+        gridSnap.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        gridSnap.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        
+        transform.position = new Vector3(gridSnap.x, 0f , gridSnap.z);
+    }
+}
