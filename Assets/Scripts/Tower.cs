6e11608aa821325507685f06d86d64106ed0d709
Chiaration <45766347+Chiaration@users.noreply.github.com>
2019-10-09 18:10:28 +0100

    Hitpoints and Enemy death

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -6,9 +6,10 @@ public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
     [SerializeField] Transform targetEnemey;
+    [SerializeField] float attackRange = 10f;
+    [SerializeField] ParticleSystem projectileParticle;
     
-    
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,33 @@ public class Tower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        objectToPan.LookAt(targetEnemey);
+        if (targetEnemey)
+        {
+            objectToPan.LookAt(targetEnemey);
+            FireAtEnemy();
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void FireAtEnemy()
+    {
+        float DistanceToEnemy = Vector3.Distance(targetEnemey.transform.position, gameObject.transform.position);
+        if (DistanceToEnemy <= attackRange)
+        {
+            Shoot(true);
+        }
+        else
+        {
+            Shoot(false);
+        }
+    }
+
+    private void Shoot(bool isActive)
+    {
+        var emissionModule = projectileParticle.emission;
+        emissionModule.enabled = isActive;
     }
 }
