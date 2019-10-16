bbb682a5bdb9d6870fdf279d2e039f3ebf7b79ce
Chiaration <45766347+Chiaration@users.noreply.github.com>
2019-10-16 15:38:31 +0100

    Refactor and Base Health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    [SerializeField] int Health = 10;
+    [SerializeField] int healthDecrease = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Health = Health - healthDecrease;
+        print(Health);
+    }
+}
