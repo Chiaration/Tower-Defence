d3497d535de6e794292128e59e6fa3172ae523b7
Chiaration <45766347+Chiaration@users.noreply.github.com>
2019-10-16 15:53:23 +0100

    Scores and HP

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,29 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] float secondsBetweenSpawns = 2f;
     [SerializeField] EnemyMovement enemyPrefab;
     [SerializeField] Transform enemyTransformParent;
+    [SerializeField] Text scoreText;
+    [SerializeField] int currentScore = 0;
+    [SerializeField] int scoreIncrease = 10;
+    
+    
     
     
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(spawnEnemy());
+        scoreText.text = "Score: 0";
     }
 
     // Update is called once per frame
     void Update()
     {
+        scoreText.text = "Score: " + currentScore;
     }
     
     private IEnumerator spawnEnemy()
     {
         while (true)
         {
+            currentScore = currentScore + scoreIncrease;
             var enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
             enemy.transform.parent = enemyTransformParent;
             yield return new WaitForSeconds(secondsBetweenSpawns);
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -3,16 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
 
     [SerializeField] int Health = 10;
     [SerializeField] int healthDecrease = 1;
+    [SerializeField] private Text healthText;
+
+    private void Start()
+    {
+        healthText.text = "HP: " + Health.ToString() + "/10";
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         Health = Health - healthDecrease;
-        print(Health);
+        healthText.text = "HP: " + Health.ToString()+ "/10";
     }
 }
